1a851d88fa1bbeb42e17cd38244f19f003b41a22
Rémi KORZENIOWSKI <korzeniowski.remi@gmail.com>
2023-11-02 12:10:43 +0100

    ci: adding API.txt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,14 @@
+import axios from "axios";
+
 export default function Home() {
+    const test = axios({
+        url:'https://pokeapi.co/api/v2/pokemon/6/',
+        method: 'get',
+        transformRequest: [function (data) {
+            return data;
+        }]
+    });
+    console.log(test);
     return (
         <div>
             <p>Hello World !</p>
