28a339b9913d679efe01cef7f5d7c42f54f02776
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2023-11-03 12:14:08 +0100

    feat: filters & scrollbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,7 +36,9 @@
         const [imgArray, setImgArray] = useState([]); // 
         const [isLoading, setIsLoading] = useState(true);
         const [searchQuery, setSearchQuery] = useState('');
-        
+        const [currentAlpha, setCurrentAlpha] = useState(false);
+        const [currentID, setCurrentID] = useState(true);
+        const [currentType, setCurrentType] = useState(false);
         useEffect(() => {
             const audio = document.getElementById('player');
             // audio.play();
@@ -53,7 +55,6 @@
                 types,
                 img: officialArtworkURL,
                 });
-                console.log(tempPkArray)
             }
             for (let current = 250; current < 500; current++) {
                 const url = `https://pokeapi.co/api/v2/pokemon/${current}`;
@@ -66,7 +67,6 @@
                 types,
                 img: officialArtworkURL,
                 });
-                console.log(tempPkArray)
             }
             for (let current = 500; current < 750; current++) {
                 const url = `https://pokeapi.co/api/v2/pokemon/${current}`;
@@ -79,7 +79,6 @@
                 types,
                 img: officialArtworkURL,
                 });
-                console.log(tempPkArray)
 
             }
             for (let current = 750; current < 1018; current++) {
@@ -93,7 +92,6 @@
                 types,
                 img: officialArtworkURL,
                 });
-                console.log(tempPkArray)
 
             }
             for (let current = 10001; current < 10264; current++) {
@@ -106,21 +104,125 @@
                 name,
                 img: officialArtworkURL,
                 });
-                console.log(tempPkArray)
 
             }
             setPkArray(tempPkArray);
             setIsLoading(false);
-            };
+        };
         
-            fetchData();
-        }, []);
+        fetchData();
+    }, []);
+    
+    const [isPopupOpen, setPopupOpen] = useState(false);
+    
+    const filteredPokemon = pkArray.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    
+        const FilteredAlpha = () => {
+            const sortedPokemon = [...filteredPokemon];
 
-        const [isPopupOpen, setPopupOpen] = useState(false);
+            if (currentAlpha === false) {
+                sortedPokemon.sort((a, b) => {
+                    if (a.name < b.name) {
+                        setCurrentAlpha(true)
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        setCurrentAlpha(true)
+                        return 1;
+                    }
+                    setCurrentAlpha(true)
+                    return 0;
+                });
+            } else if (currentAlpha === true){
+                sortedPokemon.sort((a, b) => {
+                    if (a.name < b.name) {
+                        setCurrentAlpha(false);
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        setCurrentAlpha(false);
+                        return -1;
+                    }
+                    setCurrentAlpha(false);
+                    return 0;
+                });
+            }
 
-        const filteredPokemon = pkArray.filter((pokemon) =>
-            pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
-        );
+            setPkArray(sortedPokemon);
+        };
+
+        const FilteredID = () => {
+            const sortedPokemon = [...filteredPokemon];
+
+            if (currentID === false) {
+                sortedPokemon.sort((a, b) => {
+                    if (a.id < b.id) {
+                        setCurrentID(true)
+                        return -1;
+                    }
+                    if (a.id > b.id) {
+                        setCurrentID(true)
+                        return 1;
+                    }
+                    setCurrentID(true)
+                    return 0;
+                });
+            } else if (currentID === true){
+                sortedPokemon.sort((a, b) => {
+                    if (a.id < b.id) {
+                        setCurrentID(false)
+                        return 1;
+                    }
+                    if (a.id > b.id) {
+                        setCurrentID(false)
+                        return -1;
+                    }
+                    setCurrentID(false)
+                    return 0;
+                });
+            }
+
+            setPkArray(sortedPokemon);
+        };
+
+        const FilteredType = () => {
+            const sortedPokemon = [...filteredPokemon];
+            sortedPokemon.sort((a, b) => {
+              let atype = (a.types && a.types[0]) ? a.types[0].type.name : "";
+              let btype = (b.types && b.types[0]) ? b.types[0].type.name : "";
+
+              if (currentType === false) {
+                    if (atype < btype) {
+                        setCurrentType(true)
+                        return -1;
+                    }
+                    if (atype > btype) {
+                        setCurrentType(true)
+                        return 1;
+                    }
+                    setCurrentType(true)
+                    return 0;
+                } else if (currentType === true) {
+                    if (atype < btype) {
+                        setCurrentType(false)
+                        return 1;
+                    }
+                    if (atype > btype) {
+                        setCurrentType(false)
+                        return -1;
+                    }
+
+                    setCurrentType(false)
+                    return 0;
+                }
+            });
+          
+            setPkArray(sortedPokemon);
+          };
+          
+          
     
 
         const openPopup = (pokemon) => {
@@ -296,156 +398,6 @@
                     break;
         }
 
-        switch(pkType1) {
-            case "bug":
-                classType1 = "bug";
-                imgType1 = bug
-                break;
-            case "fighting":
-                classType1 = "fight";
-                imgType1 = fight
-                break;
-            case "psychic":
-                classType1 = "psy";
-                imgType1 = psy
-                break;
-            case "poison":
-                classType1 = "poison";
-                imgType1 = poison
-                break;
-            case "dragon":
-                classType1 = "dragon";
-                imgType1 = dragon
-                break;
-            case "ghost":
-                classType1 = "ghost";
-                imgType1 = ghost
-                break;
-            case "dark":
-                classType1 = "dark";
-                imgType1 = dark
-                break;
-            case "ground":
-                classType1 = "ground";
-                imgType1 = ground
-                break;
-            case "fire":
-                classType1 = "fire";
-                imgType1 = fire
-                break;
-            case "fairy":
-                classType1 = "fairy";
-                imgType1 = fairy
-                break;
-            case "water":
-                classType1 = "water";
-                imgType1 = water
-                break;
-            case "fly":
-                classType1 = "fly";
-                imgType1 = fly
-                break;
-            case "normal":
-                classType1 = "normal";
-                imgType1 = normal
-                break;
-            case "steel":
-                classType1 = "steel";
-                imgType1 = steel
-                break;
-            case "rock":
-                classType1 = "rock";
-                imgType1 = rock
-                break;
-            case "electric":
-                classType1 = "electric";
-                imgType1 = electric
-                break;
-            case "grass":
-                classType1 = "grass";
-                imgType1 = grass
-                break;
-            case "ice":
-                classType1 = "ice";
-                imgType1 = ice
-                break;
-        }
-
-            switch(pkType2) {
-                case "bug":
-                    classType2 = "bug";
-                    imgType2 = bug
-                    break;
-                case "fighting":
-                    classType2 = "fight";
-                    imgType2 = fight
-                    break;
-                case "psy":
-                    classType2 = "psy";
-                    imgType2 = fight
-                    break;
-                case "poison":
-                    classType2 = "poison";
-                    imgType2 = poison
-                    break;
-                case "dragon":
-                    classType2 = "dragon";
-                    imgType2 = dragon
-                    break;
-                case "ghost":
-                    classType2 = "ghost";
-                    imgType2 = ghost
-                    break;
-                case "dark":
-                    classType2 = "dark";
-                    imgType2 = dark
-                    break;
-                case "ground":
-                    classType2 = "ground";
-                    imgType2 = ground
-                    break;
-                case "fire":
-                    classType2 = "fire";
-                    imgType2 = fire
-                    break;
-                case "fairy":
-                    classType2 = "fairy";
-                    imgType2 = fairy
-                    break;
-                case "water":
-                    classType2 = "water";
-                    imgType2 = water
-                    break;
-                case "flying":
-                    classType2 = "fly";
-                    imgType2 = fly
-                    break;
-                case "normal":
-                    classType2 = "normal";
-                    imgType2 = normal
-                    break;
-                case "steel":
-                    classType2 = "steel";
-                    imgType2 = steel
-                    break;
-                case "rock":
-                    classType2 = "rock";
-                    imgType2 = rock
-                    break;
-                case "electric":
-                    classType2 = "electric";
-                    imgType2 = electric
-                    break;
-                case "grass":
-                    classType2 = "grass";
-                    imgType2 = grass
-                    break;
-                case "ice":
-                    classType2 = "ice";
-                    imgType2 = ice
-                    break;
-        }
-
         return (
             <div className='content'>
                 <audio autoPlay id="player" src={music} loop></audio>
@@ -542,13 +494,13 @@
                     <input type="text" placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
                 </div>
                 <div className="filter">
-                    <div className="alpha" id='btn'>
+                    <div className="alpha" id='btn' onClick={FilteredAlpha}>
                         <p>Alphabetics</p>
                     </div>
-                    <div className="type" id='btn'>
+                    <div className="type" id='btn' onClick={FilteredType}>
                         <p>Type</p>
                     </div>
-                    <div id='btn'>
+                    <div id='btn' onClick={FilteredID}>
                         <p>ID</p>
                     </div>
                 </div>
